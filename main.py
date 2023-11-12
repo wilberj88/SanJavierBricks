7cb3f5cba745e3a514e48dd8c9080227436fd07e
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-12 17:52:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -449,6 +449,64 @@ if a and b and c:
     colx, coly = st.columns(2)
     with colx:
         st.subheader("Alarma de Inflación Proyectada 12 meses")
+        def render_basic_radar():
+            option = {
+                "title": {"text": "Zonas Alcaldía Valledupar 🗳️"},
+                "legend": {"data": ["Ganador 2015", "2do 2015", "3ro 2015", "Ganador 2019", "2do 2019", "3ro 2019"]},
+                "radar": {
+                    "indicator": [
+                        {"name": "Zona 1", "max": 14000},
+                        {"name": "Zona 2", "max": 14000},
+                        {"name": "Zona 3", "max": 14000},
+                        {"name": "Zona 4", "max": 14000},
+                        {"name": "Zona 5", "max": 14000},
+                        {"name": "Zona 6", "max": 14000},
+                        {"name": "Zona 7", "max": 14000},
+                        {"name": "Zona 8", "max": 14000},
+                        {"name": "Zona 9", "max": 14000},
+                    ]
+                },
+                "series": [
+                    {
+                        "name": "Votos por Zonas",
+                        "type": "radar",
+                        "data": [
+                            {
+                                "value": [10898, 12260, 10461, 13667, 11792, 4659, 4364, 152, 5931],
+                                "name": "Ganador 2015",
+                            },
+                            {
+                                "value": [7055, 6004, 5288, 6842, 7077, 4127, 2267, 58, 3337],
+                                "name": "2do 2015",
+                            },
+                            {
+                                "value": [2273, 2792, 2419, 2954, 2437, 974, 916, 16, 2863],
+                                "name": "3ro 2015",
+                            },
+                            {
+                                "value": [9549, 11314, 10390, 12513, 12728, 4977, 2935, 84, 8095],
+                                "name": "Ganador 2019",
+                            },
+                            {
+                                "value": [10013, 9513, 9712, 11971, 12824, 5638, 2562, 80, 7957],
+                                "name": "2do 2019",
+                            },
+                            {
+                                "value": [2055, 4196, 4228, 4887, 3776, 678, 1143, 39, 528],
+                                "name": "3ro 2019",
+                            },
+                        ],
+                    }
+                ],
+            }
+            st_echarts(option, height="500px")
+        ST_RADAR_DEMOS = {
+            "Radar: Basic Radar": (
+                render_basic_radar,
+                "https://echarts.apache.org/examples/en/editor.html?c=radar",
+            ),
+        }
+        render_basic_radar()
     with coly:
         fig1 = go.Figure(data=[go.Sankey(
           node = dict(
