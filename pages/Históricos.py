8ba86b7b0a19e170b02cd66d5d1dccade70e4605
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-13 17:51:33 +0100

    Create Históricos.py

---
+++ pages/Históricos.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from streamlit_extras.let_it_rain import rain 
+from streamlit_echarts import st_echarts
+import plotly.graph_objects as go
+import folium
+import time
+import datetime
+import pandas as pd
+import numpy as np
+from PIL import Image
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.grid import grid
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="SanJavierBricks demo Novus", page_icon="🧱")
+
+with st.sidebar:
+    image = Image.open('SanJavierBricks.jpeg')
+    st.image(image, caption='Demo Novus Mando: Monitor en Tiempo Real de Productividad Actual Vs Histórica. Alarmas y Recomendaciones para Planta Zamora 🏭')
+
+st.title('Central de Datos Históricos - Planta Zamora 🧱')
+
+
+st.write("Resultados E-14 por Zonas")
+options = {
+    "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+    "legend": {
+        "data": ["Ganador_Ramirez", "2do_Gonzalez", "3ro_Fernandez"]
+    },
+    "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+    "xAxis": {"type": "value"},
+    "yAxis": {
+        "type": "category",
+        "data": ["Zona 1", "Zona 2", "Zona 3", "Zona 4", "Zona 5", "Zona 6", "Zona 7", "Zona 8", "Zona 9"],
+    },
+    "series": [
+        {
+            "name": "Ganador_Ramirez",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [10898, 12260, 10461, 13667, 11792, 4659, 4364, 152, 5931],
+        },
+        {
+            "name": "2do_Gonzalez",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [7055, 6004, 5288, 6842, 7077, 4127, 2267, 58, 3337],
+        },
+        {
+            "name": "3ro_Fernandez",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [2273, 2792, 2419, 2954, 2437, 974, 916, 16, 2863],
+        },
+    ],
+}
+st_echarts(options=options, height="500px")
